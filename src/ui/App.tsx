0839a269376308e6fb57875beb2148bc7b28dd57
James Pearce <github@tripleodeon.com>
2024-05-12 15:06:37 -0600

    [setup] TinyBase basics

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,3 +1,17 @@
+import {Provider, useCreateStore} from 'tinybase/debug/ui-react';
+import {Button} from './Button';
 import React from 'react';
+import {StoreInspector} from 'tinybase/debug/ui-react-dom';
+import {createStore} from 'tinybase/debug';
 
-export const App = () => <div id="App">It works</div>;
+export const App = () => {
+  const store = useCreateStore(createStore);
+
+  return (
+    <Provider store={store}>
+      It works.
+      <Button />
+      <StoreInspector />
+    </Provider>
+  );
+};
