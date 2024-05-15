820490914c92729341bc69d63def40070f040aff
James Pearce <github@tripleodeon.com>
2024-05-15 15:44:28 -0500

    [store] Load repo data from GitHub

--- src/stores/RepoStore.tsx
+++ src/stores/RepoStore.tsx
@@ -4,68 +4,122 @@ import {
   REPOS_STORE,
   REPOS_TABLE,
 } from './ReposStore';
-import {type Values, createCustomPersister, createStore} from 'tinybase/debug';
+import {
+  type Store,
+  type Values,
+  createCustomPersister,
+  createStore,
+} from 'tinybase/debug';
 import {
   useCell,
   useCreatePersister,
   useCreateStore,
   useProvideStore,
 } from 'tinybase/debug/ui-react';
+import {Octokit} from 'octokit';
 import {createLocalPersister} from 'tinybase/debug/persisters/persister-browser';
+
+const octokit = new Octokit({});
 
 export const REPO_STORE = 'repo';
 
 export const OWNER_VALUE = 'owner';
 export const REPO_VALUE = 'repo';
-export const OWNER_AVATAR_VALUE = 'ownerAvatar';
+export const OWNER_AVATAR_URL_VALUE = 'ownerAvatarUrl';
 export const DESCRIPTION_VALUE = 'description';
 export const FORK_VALUE = 'fork';
 export const HOMEPAGE_VALUE = 'homepage';
-export const STARGAZERS_VALUE = 'stargazers';
+export const STARGAZERS_COUNT_VALUE = 'stargazersCount';
 export const LANGUAGE_VALUE = 'language';
 
 export const REFRESH_INTERVAL = 60000;
 
 export const RepoStore = ({repoId}: {readonly repoId: string}) => {
-  const owner = useCell(REPOS_TABLE, repoId, REPOS_OWNER_CELL, REPOS_STORE);
-  const repo = useCell(REPOS_TABLE, repoId, REPOS_REPO_CELL, REPOS_STORE);
+  const owner = useCell(
+    REPOS_TABLE,
+    repoId,
+    REPOS_OWNER_CELL,
+    REPOS_STORE,
+  ) as string;
+  const repo = useCell(
+    REPOS_TABLE,
+    repoId,
+    REPOS_REPO_CELL,
+    REPOS_STORE,
+  ) as string;
 
-  const repoStore = useCreateStore(createStore);
+  const repoStore = useCreateStore(createStore, [owner, repo]);
   useCreatePersister(
     repoStore,
-    (repoStore) =>
-      createLocalPersister(repoStore, owner + '/' + repo + '/' + REPO_STORE),
+    (repoStore) => {
+      if (owner && repo) {
+        return createLocalPersister(
+          repoStore,
+          owner + '/' + repo + '/' + REPO_STORE,
+        );
+      }
+    },
     [owner, repo],
     async (persister) => {
-      await persister.startAutoLoad();
-      await persister.startAutoSave();
+      await persister?.startAutoLoad();
+      await persister?.startAutoSave();
     },
+    [],
   );
 
   useCreatePersister(
     repoStore,
-    (repoStore) =>
-      createCustomPersister(
-        repoStore,
-        async () => {
-          return [
-            {},
-            {
-              [OWNER_VALUE]: owner,
-              [REPO_VALUE]: repo,
-            } as Values,
-          ];
-        },
-        async () => {},
-        (listener) => setInterval(listener, REFRESH_INTERVAL),
-        (intervalId) => clearInterval(intervalId),
-      ),
+    (repoStore) => {
+      if (owner && repo) {
+        return createGithubRepoLoadingPersister(repoStore, owner, repo);
+      }
+    },
     [owner, repo],
     async (persister) => {
-      await persister.startAutoLoad();
+      await persister?.startAutoLoad();
     },
+    [],
   );
 
   useProvideStore(REPO_STORE, repoStore);
   return null;
 };
+
+const createGithubRepoLoadingPersister = (
+  store: Store,
+  owner: string,
+  repo: string,
+) =>
+  createCustomPersister(
+    store,
+    async () => {
+      const response = await octokit.rest.repos.get({owner, repo});
+      if (response.status == 200) {
+        const {
+          owner,
+          name,
+          description,
+          fork,
+          homepage,
+          stargazers_count,
+          language,
+        } = response.data;
+        return [
+          {},
+          {
+            [OWNER_VALUE]: owner.login,
+            [REPO_VALUE]: name,
+            [OWNER_AVATAR_URL_VALUE]: owner.avatar_url,
+            [DESCRIPTION_VALUE]: description,
+            [FORK_VALUE]: fork,
+            [HOMEPAGE_VALUE]: homepage,
+            [STARGAZERS_COUNT_VALUE]: stargazers_count,
+            [LANGUAGE_VALUE]: language,
+          } as Values,
+        ];
+      }
+    },
+    async () => {},
+    (listener) => setInterval(listener, REFRESH_INTERVAL),
+    (intervalId) => clearInterval(intervalId),
+  );
