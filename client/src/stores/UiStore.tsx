41e9f8bc12fde431887ec20c28077bc921f86cdc
James Pearce <github@tripleodeon.com>
2024-05-20 11:29:12 -0500

    [ui] Remove hash

--- client/src/stores/UiStore.tsx
+++ client/src/stores/UiStore.tsx
@@ -1,9 +1,7 @@
-import {useCallback, useEffect} from 'react';
 import {
   useCreatePersister,
   useCreateStore,
   useProvideStore,
-  useValueListener,
 } from 'tinybase/debug/ui-react';
 import {createLocalPersister} from 'tinybase/debug/persisters/persister-browser';
 import {createStore} from 'tinybase/debug';
@@ -27,28 +25,6 @@ export const UiStore = () => {
       await persister.startAutoLoad();
       await persister.startAutoSave();
     },
-  );
-
-  const handleHash = useCallback(
-    () =>
-      uiStore.setValue(
-        REPO_ID_VALUE,
-        decodeURIComponent(location.hash.slice(1)),
-      ),
-    [uiStore],
-  );
-  useEffect(() => {
-    handleHash();
-    addEventListener('hashchange', handleHash);
-    return () => removeEventListener('hashchange', handleHash);
-  }, [handleHash]);
-  useValueListener(
-    REPO_ID_VALUE,
-    (_, _valueId, repoId) =>
-      history.replaceState(null, '', '#' + encodeURIComponent(repoId ?? '')),
-    [],
-    false,
-    uiStore,
   );
 
   useProvideStore(UI_STORE, uiStore);
