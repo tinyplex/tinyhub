0558fe48bb12d24d177614d99a5213a7a7ecdec0
James Pearce <github@tripleodeon.com>
2024-09-04 16:30:27 -0500

    Inspector

--- client/src/ui/index.tsx
+++ client/src/ui/index.tsx
@@ -1,23 +1,28 @@
 /** @jsx createElement */
+/** @jsxFrag Fragment */
 
+import {Fragment, createElement} from './common';
 import {Auth} from './Auth';
+import {Inspector} from 'tinybase/ui-react-inspector';
 import {Repo} from './Repo';
 import {SideNav} from './SideNav';
 import {Title} from './Title';
 import {USER_STORE} from '../stores/UserStore';
 import {Ui as UiBase} from 'tinywidgets';
 import {Welcome} from './Welcome';
-import {createElement} from './common';
 import {useHasValues} from 'tinybase/ui-react';
 
 export const Ui = () => {
   const hasUser = useHasValues(USER_STORE);
   return (
-    <UiBase
-      title={<Title />}
-      topNavRight={<Auth />}
-      sideNav={hasUser ? <SideNav /> : null}
-      article={hasUser ? <Repo /> : <Welcome />}
-    />
+    <>
+      <UiBase
+        title={<Title />}
+        topNavRight={<Auth />}
+        sideNav={hasUser ? <SideNav /> : null}
+        article={hasUser ? <Repo /> : <Welcome />}
+      />
+      <Inspector />
+    </>
   );
 };
